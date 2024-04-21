d0ea4bf3e3453c421a8edd5c0c3b2c87c3899b17
Juju Nakarmi <152660757+Jujunakarmi@users.noreply.github.com>
2024-04-21 16:19:22 -0400

    Added read write files in apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const fs = require('fs')
 const app = require('express').Router()
+const { v4: uuidv4 } = require('uuid');
+
 
 
 //Get file to read db.json file and return all save note in json
@@ -18,7 +20,12 @@ app.post('/notes', (req, res) => {
             console.log(err)
         }
         const parseData = JSON.parse(data)
-        parseData.push(newNote)
+        const updatedNote={
+            title: newNote.title,
+            text : newNote.text,
+            id: uuidv4()
+        }
+        parseData.push(updatedNote)
 
         fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(parseData), (err, data) => {
             if (err) {
@@ -31,5 +38,14 @@ app.post('/notes', (req, res) => {
     })
 
 });
+
+app.delete('/notes/:id',(req,res) => {
+    // read the id using req.params 
+    //read data from the file using fs.read
+    //convert it to object fromat usong json.parse
+    //use array.filter method to filter all the objects which are not euqls to the id parse in req.params
+    //use fs.write to  update file with new data
+    //res.status.json to send the appropraite status and new json data
+})
 
 module.exports = app;
